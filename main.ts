1269ded1965879032e281b80b62732ceaf51680d
wpakching <115679766+wpakching@users.noreply.github.com>
2022-10-13 11:16:10 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+let button_a = false
+let subtract = 0
+input.onButtonPressed(Button.A, function () {
+    if (button_a == true) {
+        button_a = false
+    } else {
+        button_a = true
+    }
+    for (let index = 0; index <= 9; index++) {
+        if (button_a == true) {
+            subtract = 9 - subtract
+            basic.showNumber(subtract)
+        } else {
+            break;
+        }
+    }
+})
 basic.forever(function () {
 	
 })
